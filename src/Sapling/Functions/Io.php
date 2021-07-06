0a76765241417690d3673aeace8fa5c81ae009a6
ShiniDev <markjoefreylaurente@gmail.com>
2021-07-06 08:38:29 +0800

    Added Sapling\Functions\Io

---
+++ src/Sapling/Functions/Io.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Sapling\Functions;
+
+class Io
+{
+    /**
+     *  Escape
+     *  Shorthand for htmlspecialchars
+     * 
+     *  @param string $message
+     *  @param int $flags
+     *  @param string $encoding
+     *  @return string
+     */
+    function esc(string $message, int $flags = ENT_QUOTES, string $encoding = 'UTF-8')
+    {
+        return htmlspecialchars($message, $flags, $encoding);
+    }
+    /**
+     *  Input
+     *  For getting input from an external data, checking if key exists and
+     *  filters it
+     *  
+     *  @param int $type
+     *  @param string $name
+     *  @param int $filter
+     */
+    function input(int $type, string $name, int $filter = FILTER_DEFAULT)
+    {
+        $res = "";
+        $res = filter_input($type, $name, $filter);
+        if ($type === FILTER_NULL_ON_FAILURE) {
+            if ($res === FALSE) {
+                Debug::debugClass("Undefined property '{$name}'");
+            } else if ($res === NULL) {
+                Debug::debugClass('Filter failed');
+            }
+        } else {
+            if ($res === NULL) {
+                Debug::debugClass("Undefined property '{$name}'");
+            } else if ($res === FALSE) {
+                Debug::debugClass('Filter failed');
+            }
+        }
+        return $res;
+    }
+}
